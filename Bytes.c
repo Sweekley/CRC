63475bfa7caf2c6060dc24eef47439a749d40aac
Scott <Darryl89@gmail.com>
2013-12-04 13:37:45 -0500

    Moved some non-used code into scratch pad.

--- Bytes.c
+++ Bytes.c
@@ -21,40 +21,21 @@ int main(void){
 	 * to figure out the CRC for LDU messages
 	 *
 	 */
-	//non-char approach
-/*	char *textMsg = "ADS072D";			//Acutal message
-	int fx[5],i=0; //= 0b10001000000100001;
-	for(i = 0; i>=5;i++){
-		fx[i] = 0b0000;
-	};
-	int test = 0b1101;	//0-8
-	puts("\n");
-	puts("Testing binary");
-	printf("f(x): %i ",test);
-*/
+
 	//Hex version
 	//char fx[16] = "10001000000100001"; 	//x^16 + x^12 + x^5 + x^0
 	//char textMsg[] = "ADS7-";			//Test text Message from 622-4
 	char *textMsg = "ADS7-";
 	//char *buff=" ";
 	int msg_length = 0;
-	char convBin[9];
-	//char textMsgHex[31] = " ";				//Test text Message in Hex
-	//char
-	//int test = 0x4B;
-	//char test[0] = "A";
-	//printf("f(x): %i \n",test);
- 	//printf("Text Message: %02X\n", test);
-	//printf("Text Message: %02X\n", &textMsg);
-	//printf("Test Message: %s\n", textMsg);
 
-	//Method above returns the location of the string me'thinks
+
 	int i,j,n;
 	i = j = n = 0;	//set to zero
 	//unsigned int ch = 0;
 	char binary[3] = " ";
 	msg_length = (sizeof(textMsg)/sizeof(textMsg[0])+1);
-	//printf("Msg length: %i\n",msg_length);
+
 	for(i = 0; i<msg_length; i++)
 	{
 		printf("%i\n",i);
@@ -79,20 +60,42 @@ int main(void){
 		printf("\n");
 		//now this needs to be put into a buffer for later
 
-		//printf("Buff: %s \n",binary);
-		//printf("Buff: %s",buff);
-		//printf("Buff: %s", buff);
-		//sprintf(buff,"0x\%x",textMsg[i]);
-		//binary = (int)strtol(textMsg,NULL,16);
-		//printf("Binary: %i",binary);
-		//printf("Buff: %s \n",buff);
-		//printf("Binary: %i \n",ch);
-		//printf("Hex: %02x\n",textMsg[i]);
-		//binary = (int)strtol(&textMsg[i],NULL,16);
-		//printf("Binary: %i\n",binary);
 
 	}
-
 	return 0;
-
 }
+/*--Scratch Pad --*/
+//non-char approach
+/*	char *textMsg = "ADS072D";			//Acutal message
+int fx[5],i=0; //= 0b10001000000100001;
+for(i = 0; i>=5;i++){
+	fx[i] = 0b0000;
+};
+int test = 0b1101;	//0-8
+puts("\n");
+puts("Testing binary");
+printf("f(x): %i ",test);
+*/
+//char textMsgHex[31] = " ";				//Test text Message in Hex
+//char
+//int test = 0x4B;
+//char test[0] = "A";
+//printf("f(x): %i \n",test);
+//printf("Text Message: %02X\n", test);
+//printf("Text Message: %02X\n", &textMsg);
+//printf("Test Message: %s\n", textMsg);
+//char convBin[9];
+//Method above returns the location of the string me'thinks
+//printf("Msg length: %i\n",msg_length);
+//printf("Buff: %s \n",binary);
+//printf("Buff: %s",buff);
+//printf("Buff: %s", buff);
+//sprintf(buff,"0x\%x",textMsg[i]);
+//binary = (int)strtol(textMsg,NULL,16);
+//printf("Binary: %i",binary);
+//printf("Buff: %s \n",buff);
+//printf("Binary: %i \n",ch);
+//printf("Hex: %02x\n",textMsg[i]);
+//binary = (int)strtol(&textMsg[i],NULL,16);
+//printf("Binary: %i\n",binary);
+
